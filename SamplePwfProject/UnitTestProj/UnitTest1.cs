0c28c08134de624080346820bc525088bf3f19ef
SlaviDimitrov <versus127@gmail.com>
2015-01-07 21:04:46 +0200

    init chekin

---
+++ SamplePwfProject/UnitTestProj/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Not implemented");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
